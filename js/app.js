e8e3029fd5c378e09801473be0ff0f6c6835fefc
ahmedali109 <ahmedalinaguib33@gmail.com>
2025-12-23 19:03:48 +0200

    refactor: restructure App function to improve welcome animation logic and container handling

--- js/app.js
+++ js/app.js
@@ -2,9 +2,17 @@ import { initWelcomeAnimation } from "./welcomeMessage.js";
 import { initRouter } from "./router.js";
 
 export function App() {
-  const { canvas } = initWelcomeAnimation(() => {
-    canvas.remove();
+  const container = document.createElement("div");
+  const currentHash = window.location.hash.slice(1);
+  const shouldShowWelcome = !currentHash || currentHash === "home";
+  if (shouldShowWelcome) {
+    const { canvas } = initWelcomeAnimation(() => {
+      canvas.remove();
+      initRouter();
+    });
+    return canvas;
+  } else {
     initRouter();
-  });
-  return canvas;
+    return container;
+  }
 }
